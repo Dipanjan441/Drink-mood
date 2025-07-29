ecd3e16a94094b696c231ef3f3bf087b14e7ade9
Dipanjan441 <dipanjantripathi441@gmail.com>
2025-07-29 10:13:25 +0530

    Navbar and Hero added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import { useGSAP } from "@gsap/react"
+import { NavbarItems } from "../constants"
+import gsap from "gsap"
+
+const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'top top',
+                toggleActions: 'play reverse play reverse'
+            }
+        })
+
+        navTween.fromTo('nav',{backgroundColor: 'transparent'},{
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut'
+        }
+        )
+    })
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="Logo" />
+                    <p>Drink Mood</p>
+                </a>
+                <ul>
+                    {NavbarItems.map((item) => (
+                        <li key={item.id}>
+                            <a href={`#${item.id}`}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
